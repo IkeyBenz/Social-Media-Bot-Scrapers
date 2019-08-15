8e33f134576107460a37db4da8f9c03f22894685
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-15 04:55:57 -0400

    Added data_to_json script in case anyone wants to visualize their social network

---
+++ data_to_json.py
@@ -0,0 +1,40 @@
+"""
+Goes through the folders in data/instagram and writes a json object, containing
+the nodes and all of their links, to data.json. Once data.json exists, you'll be
+able to open index.html and see a D3.js visualization of your social network.
+
+To be able to run this as a script right out of the box, replace all 'ikeybenz'
+in this file, with your instagram username.
+"""
+import json
+from os import listdir, path
+
+data_dir = "../SocialMediaScrappers/GitRepo/data/instagram"
+ikeys_connections = set(open(
+    data_dir + '/ikeybenz/connections.txt').read().splitlines())
+
+
+def get_links():
+    links = []
+    for followed in listdir(data_dir):
+        mutuals_path = f"{data_dir}/{followed}/mutuals_with_ikeybenz.txt"
+        if followed is "ikeybenz" or not path.exists(mutuals_path):
+            continue
+
+        for follower in open(mutuals_path).read().splitlines():
+            links.append({"source": follower, "target": followed})
+    print("Links", len(links))
+    return links
+
+
+if __name__ == '__main__':
+    data = {
+        "nodes": [
+            {"id": acc, "group": 1}
+            for acc in ikeys_connections
+        ] + [{"id": "ikeybenz", "group": 1}],
+        "links": get_links()[:18000]
+    }
+
+    with open('data.json', 'w') as out:
+        json.dump(data, out)
